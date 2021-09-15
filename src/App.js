eb75e047be25397b698dc398451811166911b9f4
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-15 14:39:59 +0530

    Webcam add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Webcam from 'react-webcam';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {/* <Webcam /> */}
     </div>
   );
 }
