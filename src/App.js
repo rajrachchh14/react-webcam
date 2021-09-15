b701e6762a5cb71ad4323e78305c8b1ea74f48a7
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-15 15:28:53 +0530

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Webcam from 'react-webcam';
 
 export default function App() {
+  // const videoConstraints = {
+  //   width: 1280,
+  //   height: 720,
+  //   facingMode: 'user'
+  // };
+
+  // const webcamRef = React.useRef(null);
+  // const [imgSrc, setImgSrc] = React.useState(null);
+
+  // const capture = React.useCallback(() => {
+  //   const imageSrc = webcamRef.current.getScreenshot();
+  //   setImgSrc(imageSrc);
+  // }, [webcamRef, setImgSrc]);
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      {/* <Webcam /> */}
-    </div>
+    <>
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a className="navbar-brand" href="#">
+          Logo
+        </a>
+
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link 1
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link 2
+            </a>
+          </li>
+
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbardrop"
+              data-toggle="dropdown"
+            />
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="#">
+                Link 1
+              </a>
+              <a className="dropdown-item" href="#">
+                Link 2
+              </a>
+              <a className="dropdown-item" href="#">
+                Link 3
+              </a>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      {/* <Webcam
+        audio={false}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      />
+      <button onClick={capture}>Capture photo</button>
+      {imgSrc && <img src={imgSrc} />} */}
+    </>
   );
 }
